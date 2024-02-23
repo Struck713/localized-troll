b9a2456e8e83573ab1fbd58d28b6f2d233fdfb70
Struck713 <noahstruck71303@gmail.com>
2024-02-23 02:12:55 -0500

    Added some more types and made bot respond to tagged and replied messages

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,52 @@
 import { createCompletion, loadModel } from "gpt4all";
-import { Message } from "./types";
+import { Message, ReferencedMessage } from "./types";
 
 import config from "../config.json";
 
 const Discord = require("discord-user-bots"); // no types
 const client = new Discord.Client(config.token);
 
+
+
 (async () => {
     const model = await loadModel(config.model, { device: "gpu", modelPath: config.model_path, verbose: true });
+
+    let semaphore = 0;
+    const consultModelForResponse = async (message: Message) => {
+        await client.type(message.channel_id);
+        const response = await createCompletion(model, [
+            { role: 'user', content: `${message.content}` }
+        ], { 
+            promptHeader: "Your name is Harold. Be as rude as possible.",
+        });
+        
+        const choice = response.choices.filter(res => res.message.content.length != 0)[0];
+        if (choice) {
+            await client.send(message.channel_id, { content: choice.message.content, reply: message.id });
+            await client.stop_type();
+        }
+        semaphore--;
+    }
 
     client.on.ready = function () {
         console.log(`${client.info.user.username} is ready to talk nonsense.`);
     };
-    
-    let semaphore = 0;
-    client.on.message_create = (message: Message) => {
-        if (message.author.id === client.info.user.id) return;
-        if (semaphore > 0) return;
 
+    client.on.reply = (message: Message) => {
+        if (!message.referenced_message) return;
+        if (message.referenced_message.author.id != client.info.user.id) return;
+        if (semaphore > 0) return;
+        
         semaphore++;
-        (async () => {
-            await client.type(message.channel_id);
-            const response = await createCompletion(model, [
-                { role: 'user', content: `${message.author.username} said "${message.content}"` }
-            ], { contextErase: 0.5, repeatPenalty: 0.5, promptHeader: "Be annoying and rude. Your name is Harold." });
-            console.log(response.usage);
-            await client.send(message.channel_id, { content: response.choices[0].message.content.trim(), reply: message.id });
-            await client.stop_type();
-            semaphore--;
-        })();
+        consultModelForResponse(message);
+    }
+    
+    client.on.message_create = (message: Message) => {
+        if (message.author.id == client.info.user.id) return;
+        if (!message.mentions.find(mention => mention.id == client.info.user.id)) return;
+        if (semaphore > 0) return;
+        
+        semaphore++;
+        consultModelForResponse(message);
     };
 })();
--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,45 @@
 
+export interface Member {
+    username: string,
+    public_flags: number,
+    premium_type: number,
+    id: string,
+    global_name: string,
+    discriminator: string,
+    avatar_decoration_data: null,
+    avatar: string
+}
+
+export interface ReferencedMessage {
+    type: number,
+    tts: boolean,
+    timestamp: Date,
+    pinned: boolean,
+    mentions: Mention[],
+    mention_roles: [],
+    mention_everyone: boolean,
+    id: string,
+    flags: number,
+    embeds: [],
+    edited_timestamp: null,
+    content: string,
+    components: [],
+    channel_id: string,
+    author: Member,
+    attachments: []
+}
+
 export interface Message {
   type: number,
   tts: boolean,
   timestamp: Date,
-  referenced_message: null,
+  referenced_message?: ReferencedMessage,
   pinned: boolean,
   nonce: string,
-  mentions: [],
+  mentions: Mention[],
   mention_roles: [],
   mention_everyone: boolean,
-  member: {
-    roles: string[],
-    premium_since: null,
-    pending: boolean,
-    nick: string,
-    mute: boolean,
-    joined_at: Date,
-    flags: number,
-    deaf: boolean,
-    communication_disabled_until: null,
-    avatar: null
-  },
+  member: Member,
   id: string,
   flags: number,
   embeds: [],
@@ -41,3 +60,14 @@ export interface Message {
   attachments: [],
   guild_id: string
 }
+
+interface Mention {
+    username: string,
+    public_flags: number,
+    member: Member,
+    id: string,
+    global_name: string,
+    discriminator: string,
+    avatar_decoration_data: null,
+    avatar?: string
+  }
